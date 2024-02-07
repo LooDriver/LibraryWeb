cd220f47a399d0dd95046a4ccf1cdda13efcec5e
Капитан Хипхоп <graycat305@gmail.com>
2024-02-07 14:29:39 +0300

    успешная интеграция typescript, сделана оптимизация кода

--- LibraryWeb.Core/Scripts/app.ts
+++ LibraryWeb.Core/Scripts/app.ts
@@ -1,8 +1,100 @@
 ﻿const baseUrl = 'api';
 
+class elementCreate {
+
+    createAboutBook(author, genre, book) {
+        $('#img-cover-about-book').attr('src', `data:image/png;base64,${book.обложкаКниги}`);
+        $('#h2-tittle-about-book').text(`${book.название}`);
+        $('#p-author-about-book').text(`${author.фио}`);
+        $('#p-genre-about-book').text(`Жанр - ${genre.названиеЖанра}`);
+        $('#p-available-about-book').text(`Наличии - ${book.количествоВНаличии} шт.`);
+
+
+    }
+
+
+    tilesFiller(books) {
+        var arr = [];
+
+        for (var i = 0; i < books.length; i++) {
+            arr.push('<div class="col-md-2">');
+            arr.push('<div class="tile">');
+            arr.push('<div class="tile-content">');
+            arr.push(`<div class="tile-book">${books[i].название}</div>`);
+            arr.push(`<button class="btn-about-book"><img src="data:image/png;base64,${books[i].обложкаКниги}" width="100" height="150" alt="Обложка книги ${books[i].название}"></button>`);
+            arr.push('</div>');
+            arr.push('</div>');
+            arr.push('</div>');
+        }
+
+        $('#tileContainer').append('<div class="row">' + arr.join('') + '</div>');
+
+    }
+}
+
+class Authentication {
+
+    private readonly defaultRole: number = 2; 
+
+    private user = {
+        Логин: "",
+        Пароль: "",
+        КодРоли: this.defaultRole
+    };
+
+
+    constructor(username: string, password: string) {
+
+        this.user.Логин = username;
+        this.user.Пароль = password;
+    }
+
+    Login() {
+        if (this.user.Логин != "" && this.user.Пароль != "") {
+            var result = false;
+            $.ajax({
+                url: `${baseUrl}/auth/login`,
+                method: 'post',
+                contentType: 'application/json;charset=utf-8',
+                data: JSON.stringify(this.user),
+                async: true
+            }).done(function (data) {
+                /*sessionStorage.setItem('auth_key', `${data}`);*/
+                result = true;
+            }).fail(function () {
+                result = false;
+            })
+            return result;
+        }
+    }
+
+    Register() {
+        if (this.user.Пароль == $('#input-form-password-repeat').val()) {
+            $.ajax({
+                url: `${baseUrl}/auth/register`,
+                method: 'post',
+                contentType: 'application/json;charset=utf-8',
+                data: JSON.stringify(this.user),
+                async: true
+            }).done(function () {
+
+            }).fail(function () {
+
+            });
+        }
+        else {
+            return 'Пароли должны быть одинаковыми';
+        }
+    }
+
+}
+
+
 $(function () {
 
     $(document).ready(function () {
+
+        var elements = new elementCreate();
         if (window.location.pathname.length == 1) {
             $.ajax({
                 url: `${baseUrl}/books/allBooks`,
@@ -11,7 +103,7 @@ $(function () {
                 dataType: 'json',
                 async: true
             }).done(function (data) {
-                tilesFiller(data);
+                elements.tilesFiller(data);
 
             }).fail(function (handleError) {
                 console.log(handleError);
@@ -19,26 +111,26 @@ $(function () {
         }
         else if (window.location.href.search('book/name') && sessionStorage.getItem('bookData') !== undefined) {
             var dataStorage = JSON.parse(sessionStorage.getItem('bookData'));
-            createAboutBook(dataStorage.author, dataStorage.genre, dataStorage.book);
+            elements.createAboutBook(dataStorage.author, dataStorage.genre, dataStorage.book);
         }
-        else if (window.location.href.search('easydata') && sessionStorage.getItem('auth_key') !== undefined) {
-        const token = sessionStorage.getItem('auth_key');
-        fetch('/easydata',
-            {
-                method: 'GET',
-                headers: {
-                    "Authorization": `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiYWRtaW5AbWFpbC5ydSIsImV4cCI6MTcwNjI3NTEyMCwiaXNzIjoiU2VydmVyIiwiYXVkIjoiQ2xpZW50In0.qrftgT9qbrtcmogEtSffQ6xwyZlf83ryPcZtGh0DBPM`,
-                    "Accept": "application/json"
-                }
-            }).then(response => {
-                if (response.ok) {
-                    window.location.href = '/easydata';
-                }
-            }).then(data => {
-            }).catch(function (err) {
-                console.log(err);
-            });
-        }
+        //else if (window.location.href.search('easydata') && sessionStorage.getItem('auth_key') !== undefined) {
+        //const token = sessionStorage.getItem('auth_key');
+        //fetch('/easydata',
+        //    {
+        //        method: 'GET',
+        //        headers: {
+        //            "Authorization": `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiYWRtaW5AbWFpbC5ydSIsImV4cCI6MTcwNjI3NTEyMCwiaXNzIjoiU2VydmVyIiwiYXVkIjoiQ2xpZW50In0.qrftgT9qbrtcmogEtSffQ6xwyZlf83ryPcZtGh0DBPM`,
+        //            "Accept": "application/json"
+        //        }
+        //    }).then(response => {
+        //        if (response.ok) {
+        //            window.location.href = '/easydata';
+        //        }
+        //    }).then(data => {
+        //    }).catch(function (err) {
+        //        console.log(err);
+        //    });
+        //}
 
     });
 
@@ -97,93 +189,21 @@ $(function () {
         });
     });
     $('#btn-form-login').on('click', function (event) {
-        let login = {
-            Логин: $('#input-form-email').val(),
-            Пароль: $('#input-form-password').val()
-        };
-        $.ajax({
-            url: `${baseUrl}/auth/enter`,
-            method: 'post',
-            contentType: 'application/json;charset=utf-8',
-            data: JSON.stringify(login),
-            async: true
-        }).done(function (data) {
-            sessionStorage.setItem('auth_key', `${data}`);
-        }).fail(function () {
-            event.preventDefault();
-        })
+        event.preventDefault();
+        var enter = new Authentication($('#input-form-email').val().toString(), $('#input-form-password').val().toString());
+
+        if (enter.Login()) {
+            alert("Успешно");
+        } else {
+            alert("Неуспешно");
+        }
     });
 
     $('#btn-form-register').on('click', function (event) {
+        event.preventDefault();
+        var register = new Authentication($('#input-form-email-register').val().toString(), $('#input-form-password-register').val().toString());
+        var res = register.Register();
+        alert(res);
 
-        let registerUser = {
-            Логин: $('#input-form-email-register').val(),
-            Пароль: $('#input-form-password-register').val(),
-            КодРоли: 2
-        }
-        if (registerUser.Пароль == $('#input-form-password-repeat').val()) {
-            $.ajax({
-                url: `${baseUrl}/auth/register`,
-                method: 'post',
-                contentType: 'application/json;charset=utf-8',
-                data: JSON.stringify(registerUser),
-                async: true
-
-            }).done(function () {
-
-            }).fail(function () {
-                event.preventDefault();
-            });
-        }
-        else {
-            event.preventDefault();
-            alert("Пароли должны быть одинаковыми");
-        }
-
-    });
-    function createAboutBook(author, genre, book) {
-        var arr = [];
-        arr.push('<div class="col-md-4 text-center">');
-        arr.push(`<img src="data:image/png;base64,${book.обложкаКниги}" alt="фото">`);
-        arr.push('</div>');
-        arr.push('<div class="col-md-8">');
-        arr.push('<div class="book-details">');
-        arr.push(`<h2>${book.название}</h2>`)
-        arr.push(`<p class="text-muted">Автор: ${author.фио}</p>`);
-        arr.push(`<p><strong>Жанр: </strong>${genre.названиеЖанра}</p>`);
-        arr.push(`<p><strong>В наличии: </strong>${book.количествоВНаличии}</p>`);
-        arr.push(`</div>`);
-        arr.push(`<div class="book-description">`);
-        arr.push(`<hr>`);
-        arr.push(`<h4>Описание</h4>`);
-        arr.push(`<p></p>`);
-        arr.push(`<hr>`);
-        arr.push(`</div>`);
-        arr.push(`<p class="text-left">`);
-        arr.push(`<a class="btn btn-primary" id="btn-about-buy">Купить</a>`);
-        arr.push(`<a class="btn btn-primary" id="btn-about-read-passage">Читать отрывок</a>`);
-        arr.push(`</p>`);
-        arr.push(`</div>`);
-        $('#div-about-book').append('<div class="row">' + arr.join('') + '</div>');
-
-    }
-
-    
-    function tilesFiller(books) {
-        var arr = [];
-
-        for (var i = 0; i < books.length; i++) {
-            arr.push('<div class="col-md-2">');
-            arr.push('<div class="tile">');
-            arr.push('<div class="tile-content">');
-            arr.push(`<div class="tile-book">${books[i].название}</div>`);
-            arr.push(`<button class="btn-about-book"><img src="data:image/png;base64,${books[i].обложкаКниги}" width="100" height="150" alt="Обложка книги ${books[i].название}"></button>`);
-            arr.push('</div>');
-            arr.push('</div>');
-            arr.push('</div>');
-        }
-
-        $('#tileContainer').append('<div class="row">' + arr.join('') + '</div>');
-
-    }
+    }); 
 });
--- LibraryWeb.Core/wwwroot/js/app.js
+++ LibraryWeb.Core/wwwroot/js/app.js
@@ -1,6 +1,80 @@
 var baseUrl = 'api';
+var elementCreate = /** @class */ (function () {
+    function elementCreate() {
+    }
+    elementCreate.prototype.createAboutBook = function (author, genre, book) {
+        $('#img-cover-about-book').attr('src', "data:image/png;base64,".concat(book.обложкаКниги));
+        $('#h2-tittle-about-book').text("".concat(book.название));
+        $('#p-author-about-book').text("".concat(author.фио));
+        $('#p-genre-about-book').text("\u0416\u0430\u043D\u0440 - ".concat(genre.названиеЖанра));
+        $('#p-available-about-book').text("\u041D\u0430\u043B\u0438\u0447\u0438\u0438 - ".concat(book.количествоВНаличии, " \u0448\u0442."));
+    };
+    elementCreate.prototype.tilesFiller = function (books) {
+        var arr = [];
+        for (var i = 0; i < books.length; i++) {
+            arr.push('<div class="col-md-2">');
+            arr.push('<div class="tile">');
+            arr.push('<div class="tile-content">');
+            arr.push("<div class=\"tile-book\">".concat(books[i].название, "</div>"));
+            arr.push("<button class=\"btn-about-book\"><img src=\"data:image/png;base64,".concat(books[i].обложкаКниги, "\" width=\"100\" height=\"150\" alt=\"\u041E\u0431\u043B\u043E\u0436\u043A\u0430 \u043A\u043D\u0438\u0433\u0438 ").concat(books[i].название, "\"></button>"));
+            arr.push('</div>');
+            arr.push('</div>');
+            arr.push('</div>');
+        }
+        $('#tileContainer').append('<div class="row">' + arr.join('') + '</div>');
+    };
+    return elementCreate;
+}());
+var Authentication = /** @class */ (function () {
+    function Authentication(username, password) {
+        this.defaultRole = 2;
+        this.user = {
+            Логин: "",
+            Пароль: "",
+            КодРоли: this.defaultRole
+        };
+        this.user.Логин = username;
+        this.user.Пароль = password;
+    }
+    Authentication.prototype.Login = function () {
+        if (this.user.Логин != "" && this.user.Пароль != "") {
+            var result = false;
+            $.ajax({
+                url: "".concat(baseUrl, "/auth/login"),
+                method: 'post',
+                contentType: 'application/json;charset=utf-8',
+                data: JSON.stringify(this.user),
+                async: true
+            }).done(function (data) {
+                /*sessionStorage.setItem('auth_key', `${data}`);*/
+                result = true;
+            }).fail(function () {
+                result = false;
+            });
+            return result;
+        }
+    };
+    Authentication.prototype.Register = function () {
+        if (this.user.Пароль == $('#input-form-password-repeat').val()) {
+            $.ajax({
+                url: "".concat(baseUrl, "/auth/register"),
+                method: 'post',
+                contentType: 'application/json;charset=utf-8',
+                data: JSON.stringify(this.user),
+                async: true
+            }).done(function () {
+            }).fail(function () {
+            });
+        }
+        else {
+            return 'Пароли должны быть одинаковыми';
+        }
+    };
+    return Authentication;
+}());
 $(function () {
     $(document).ready(function () {
+        var elements = new elementCreate();
         if (window.location.pathname.length == 1) {
             $.ajax({
                 url: "".concat(baseUrl, "/books/allBooks"),
@@ -9,32 +83,33 @@ $(function () {
                 dataType: 'json',
                 async: true
             }).done(function (data) {
-                tilesFiller(data);
+                elements.tilesFiller(data);
             }).fail(function (handleError) {
                 console.log(handleError);
             });
         }
         else if (window.location.href.search('book/name') && sessionStorage.getItem('bookData') !== undefined) {
             var dataStorage = JSON.parse(sessionStorage.getItem('bookData'));
-            createAboutBook(dataStorage.author, dataStorage.genre, dataStorage.book);
+            elements.createAboutBook(dataStorage.author, dataStorage.genre, dataStorage.book);
         }
-        else if (window.location.href.search('easydata') && sessionStorage.getItem('auth_key') !== undefined) {
-            var token = sessionStorage.getItem('auth_key');
-            fetch('/easydata', {
-                method: 'GET',
-                headers: {
-                    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiYWRtaW5AbWFpbC5ydSIsImV4cCI6MTcwNjI3NTEyMCwiaXNzIjoiU2VydmVyIiwiYXVkIjoiQ2xpZW50In0.qrftgT9qbrtcmogEtSffQ6xwyZlf83ryPcZtGh0DBPM",
-                    "Accept": "application/json"
-                }
-            }).then(function (response) {
-                if (response.ok) {
-                    window.location.href = '/easydata';
-                }
-            }).then(function (data) {
-            }).catch(function (err) {
-                console.log(err);
-            });
-        }
+        //else if (window.location.href.search('easydata') && sessionStorage.getItem('auth_key') !== undefined) {
+        //const token = sessionStorage.getItem('auth_key');
+        //fetch('/easydata',
+        //    {
+        //        method: 'GET',
+        //        headers: {
+        //            "Authorization": `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiYWRtaW5AbWFpbC5ydSIsImV4cCI6MTcwNjI3NTEyMCwiaXNzIjoiU2VydmVyIiwiYXVkIjoiQ2xpZW50In0.qrftgT9qbrtcmogEtSffQ6xwyZlf83ryPcZtGh0DBPM`,
+        //            "Accept": "application/json"
+        //        }
+        //    }).then(response => {
+        //        if (response.ok) {
+        //            window.location.href = '/easydata';
+        //        }
+        //    }).then(data => {
+        //    }).catch(function (err) {
+        //        console.log(err);
+        //    });
+        //}
     });
     $('#btn-test-auth').on('click', function (event) {
         event.preventDefault();
@@ -85,83 +160,20 @@ $(function () {
         });
     });
     $('#btn-form-login').on('click', function (event) {
-        var login = {
-            Логин: $('#input-form-email').val(),
-            Пароль: $('#input-form-password').val()
-        };
-        $.ajax({
-            url: "".concat(baseUrl, "/auth/enter"),
-            method: 'post',
-            contentType: 'application/json;charset=utf-8',
-            data: JSON.stringify(login),
-            async: true
-        }).done(function (data) {
-            sessionStorage.setItem('auth_key', "".concat(data));
-        }).fail(function () {
-            event.preventDefault();
-        });
-    });
-    $('#btn-form-register').on('click', function (event) {
-        var registerUser = {
-            Логин: $('#input-form-email-register').val(),
-            Пароль: $('#input-form-password-register').val(),
-            КодРоли: 2
-        };
-        if (registerUser.Пароль == $('#input-form-password-repeat').val()) {
-            $.ajax({
-                url: "".concat(baseUrl, "/auth/register"),
-                method: 'post',
-                contentType: 'application/json;charset=utf-8',
-                data: JSON.stringify(registerUser),
-                async: true
-            }).done(function () {
-            }).fail(function () {
-                event.preventDefault();
-            });
+        event.preventDefault();
+        var enter = new Authentication($('#input-form-email').val().toString(), $('#input-form-password').val().toString());
+        if (enter.Login()) {
+            alert("Успешно");
         }
         else {
-            event.preventDefault();
-            alert("Пароли должны быть одинаковыми");
+            alert("Неуспешно");
         }
     });
-    function createAboutBook(author, genre, book) {
-        var arr = [];
-        arr.push('<div class="col-md-4 text-center">');
-        arr.push("<img src=\"data:image/png;base64,".concat(book.обложкаКниги, "\" alt=\"\u0444\u043E\u0442\u043E\">"));
-        arr.push('</div>');
-        arr.push('<div class="col-md-8">');
-        arr.push('<div class="book-details">');
-        arr.push("<h2>".concat(book.название, "</h2>"));
-        arr.push("<p class=\"text-muted\">\u0410\u0432\u0442\u043E\u0440: ".concat(author.фио, "</p>"));
-        arr.push("<p><strong>\u0416\u0430\u043D\u0440: </strong>".concat(genre.названиеЖанра, "</p>"));
-        arr.push("<p><strong>\u0412 \u043D\u0430\u043B\u0438\u0447\u0438\u0438: </strong>".concat(book.количествоВНаличии, "</p>"));
-        arr.push("</div>");
-        arr.push("<div class=\"book-description\">");
-        arr.push("<hr>");
-        arr.push("<h4>\u041E\u043F\u0438\u0441\u0430\u043D\u0438\u0435</h4>");
-        arr.push("<p></p>");
-        arr.push("<hr>");
-        arr.push("</div>");
-        arr.push("<p class=\"text-left\">");
-        arr.push("<a class=\"btn btn-primary\" id=\"btn-about-buy\">\u041A\u0443\u043F\u0438\u0442\u044C</a>");
-        arr.push("<a class=\"btn btn-primary\" id=\"btn-about-read-passage\">\u0427\u0438\u0442\u0430\u0442\u044C \u043E\u0442\u0440\u044B\u0432\u043E\u043A</a>");
-        arr.push("</p>");
-        arr.push("</div>");
-        $('#div-about-book').append('<div class="row">' + arr.join('') + '</div>');
-    }
-    function tilesFiller(books) {
-        var arr = [];
-        for (var i = 0; i < books.length; i++) {
-            arr.push('<div class="col-md-2">');
-            arr.push('<div class="tile">');
-            arr.push('<div class="tile-content">');
-            arr.push("<div class=\"tile-book\">".concat(books[i].название, "</div>"));
-            arr.push("<button class=\"btn-about-book\"><img src=\"data:image/png;base64,".concat(books[i].обложкаКниги, "\" width=\"100\" height=\"150\" alt=\"\u041E\u0431\u043B\u043E\u0436\u043A\u0430 \u043A\u043D\u0438\u0433\u0438 ").concat(books[i].название, "\"></button>"));
-            arr.push('</div>');
-            arr.push('</div>');
-            arr.push('</div>');
-        }
-        $('#tileContainer').append('<div class="row">' + arr.join('') + '</div>');
-    }
+    $('#btn-form-register').on('click', function (event) {
+        event.preventDefault();
+        var register = new Authentication($('#input-form-email-register').val().toString(), $('#input-form-password-register').val().toString());
+        var res = register.Register();
+        alert(res);
+    });
 });
 //# sourceMappingURL=app.js.map
