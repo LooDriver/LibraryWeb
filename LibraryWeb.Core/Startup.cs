89ced55c43350ebd9b730363d08d729e430663b8
Капитан Хипхоп <graycat305@gmail.com>
2024-02-17 13:52:10 +0300

    успешная интеграция xUnit для проекта LibraryWeb.Core, также был создан класс "Startup.cs" для настройки веб-приложения, был почищен "Program.cs"

---
+++ LibraryWeb.Core/Startup.cs
@@ -0,0 +1,73 @@
+﻿using EasyData.Services;
+using LibraryWeb.Sql.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+
+namespace LibraryWeb.Core
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews().AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+            services.AddDbContext<DatabaseEntities>(options =>
+            {
+                options.UseSqlServer("Server=localhost\\sqlexpress;Database=Библиотека;Trusted_Connection=true;TrustServerCertificate=true");
+            });
+            //services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            //    .AddJwtBearer(options =>
+            //    {
+            //        options.TokenValidationParameters = new TokenValidationParameters
+            //        {
+            //            ValidateIssuerSigningKey = true,
+            //            ValidIssuer = AuthOptions.ISSUER,
+            //            ValidAudience = AuthOptions.AUDIENCE,
+
+            //            IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+            //            ValidateIssuer = true,  // изменено
+            //            ValidateAudience = true,
+            //            ValidateLifetime = true,
+            //            ClockSkew = TimeSpan.Zero
+            //        };
+            //    });
+            //services.AddAuthorization();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseDefaultFiles();
+
+            app.UseRouting();
+            //app.UseAuthentication();
+            //app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapEasyData(options =>
+                {
+                    options.UseDbContext<DatabaseEntities>();
+                });
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ LibraryWeb.IntegrationTest/Class1.cs
@@ -0,0 +1,32 @@
+﻿using LibraryWeb.Core;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System.Net;
+using Xunit;
+
+namespace LibraryWeb.IntegrationTest
+{
+    public class Class1
+    {
+        private HttpClient _client;
+
+        public Class1()
+        {
+            var server = new TestServer(new WebHostBuilder()
+                .UseStartup<Startup>());
+            _client = server.CreateClient();
+        }
+
+        [Theory]
+        [InlineData("GET")]
+        public async Task BooksGetAllTestAsync(string method)
+        {
+            var request = new HttpRequestMessage(new HttpMethod(method), "/api/books/allBooks");
+
+            var response = await _client.SendAsync(request);
+
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
