cd5148dcbd058e8b20d433cb04fa23a72b138926
Капитан Хипхоп <graycat305@gmail.com>
2024-02-17 15:10:47 +0300

    Добавление еще одного теста, структуризация файлов по папкам

--- LibraryWeb.IntegrationTest/ApiTest/BooksApiTest.cs
+++ LibraryWeb.IntegrationTest/ApiTest/BooksApiTest.cs
@@ -6,11 +6,11 @@ using Xunit;
 
 namespace LibraryWeb.IntegrationTest
 {
-    public class Class1
+    public class BooksApiTest
     {
         private HttpClient _client;
 
-        public Class1()
+        public BooksApiTest()
         {
             var server = new TestServer(new WebHostBuilder()
                 .UseStartup<Startup>());
@@ -22,6 +22,18 @@ namespace LibraryWeb.IntegrationTest
         public async Task BooksGetAllTestAsync(string method)
         {
             var request = new HttpRequestMessage(new HttpMethod(method), "/api/books/allBooks");
+
+            var response = await _client.SendAsync(request);
+
+            response.EnsureSuccessStatusCode();
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Theory]
+        [InlineData("GET", "Идиот")]
+        public async Task BooksGetById(string method, string name)
+        {
+            var request = new HttpRequestMessage(new HttpMethod(method), $"/book/name?{name}");
 
             var response = await _client.SendAsync(request);
 
