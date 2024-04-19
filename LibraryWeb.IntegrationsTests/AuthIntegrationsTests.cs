01d00bfd8e716225eb3992828fbb43ca698290ed
Капитан Хипхоп <graycat305@gmail.com>
2024-04-19 11:53:00 +0300

    Добавление интеграционных тестов

---
+++ LibraryWeb.IntegrationsTests/AuthIntegrationsTests.cs
@@ -0,0 +1,102 @@
+﻿using LibraryWeb.IntegrationsTests.SetupEnviroment;
+using System.Net;
+using System.Net.Http.Headers;
+
+namespace LibraryWeb.IntegrationsTests
+{
+    public class AuthIntegrationsTests : IClassFixture<BaseTestServerFixture>
+    {
+        private readonly BaseTestServerFixture _fixture;
+
+        public AuthIntegrationsTests(BaseTestServerFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public async Task Success_Login_ExistUser_Json_Async()
+        {
+            var formData = new Dictionary<string, string>
+            {
+                { "Имя", "Пользователь 2" },
+                { "Фамилия", "Фамилия 2" },
+                { "Логин", "user2" },
+                { "Пароль", "password2" }
+            };
+
+            var content = new FormUrlEncodedContent(formData);
+
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
+
+            var response = await _fixture.Client.PostAsync("/api/auth/login", content);
+
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            Assert.NotNull(responseBody);
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Unauthorized_Login_NotExist_Json_Async()
+        {
+            var formData = new Dictionary<string, string>
+            {
+                { "Имя", "Пользователь 2" },
+                { "Фамилия", "Фамилия 2" },
+                { "Логин", "" },
+                { "Пароль", "" }
+            };
+
+            var content = new FormUrlEncodedContent(formData);
+
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
+
+            var response = await _fixture.Client.PostAsync("/api/auth/login", content);
+
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            Assert.NotNull(responseBody);
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Success_RegisterNewUser_Async()
+        {
+            var formData = new Dictionary<string, string>
+            {
+                {"Имя", "Новый пользователь 1" },
+                {"Фамилия", "Новый пользователь 1" },
+                {"Логин", "newuser1" },
+                {"Пароль", "password123" },
+            };
+
+            var content = new FormUrlEncodedContent(formData);
+
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
+
+            var response = await _fixture.Client.PostAsync("/api/auth/register", content);
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Bad_RegisterNewUser_Async()
+        {
+            var formData = new Dictionary<string, string>
+            {
+                {"Имя", "Новый пользователь 1" },
+                {"Фамилия", "Новый пользователь 1" },
+                {"Логин", "" },
+                {"Пароль", "" },
+            };
+
+            var content = new FormUrlEncodedContent(formData);
+
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
+
+            var response = await _fixture.Client.PostAsync("/api/auth/register", content);
+
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+    }
+}
---
+++ LibraryWeb.IntegrationsTests/CartIntegrationsTests.cs
@@ -0,0 +1,161 @@
+﻿using LibraryWeb.IntegrationsTests.SetupEnviroment;
+using LibraryWeb.Sql.Models;
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http.Headers;
+
+namespace LibraryWeb.IntegrationsTests
+{
+    public class CartIntegrationsTests : IClassFixture<BaseTestServerFixture>
+    {
+        private readonly BaseTestServerFixture _fixture;
+
+        public CartIntegrationsTests(BaseTestServerFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [InlineData("3")]
+        [Theory]
+        public async Task GetAll_CartItems_Json_Async(string userID)
+        {
+            var response = await _fixture.Client.GetAsync($"/api/cart/allCartItems?userID={userID}");
+
+            var responseBody = await response.Content.ReadAsStringAsync();
+
+            var cartItems = JsonConvert.DeserializeObject<List<Корзина>>(responseBody);
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.NotEmpty(cartItems);
+        }
+
+        [InlineData("3", "Книга 1")]
+        [Theory]
+        public async Task Success_CheckExist_CartItem_Async(string userID, string bookName)
+        {
+            var response = await _fixture.Client.GetAsync($"/api/cart/existCartItem?userID={userID}&bookName={bookName}");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [InlineData("5", "Книга 1")]
+        [Theory]
+        public async Task Bad_CheckExist_CartItem_Async(string userID, string bookName)
+        {
+            var response = await _fixture.Client.GetAsync($"/api/cart/existCartItem?userID={userID}&bookName={bookName}");
+
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [InlineData("Книга 1", "2", "1")]
+        [Theory]
+        public async Task Success_AddNew_CartItem_Async(string bookName, string userID, string quantity)
+        {
+            var formData = new Dictionary<string, string>
+            {
+                {"bookName", bookName },
+                {"userID", userID },
+                {"quantity", quantity}
+            };
+
+            await TestAddCartItem(formData, HttpStatusCode.OK);
+        }
+
+        [InlineData("", "", "")]
+        [Theory]
+        public async Task Bad_AddNew_CartItem_Async(string bookName, string userID, string quantity)
+        {
+            var formData = new Dictionary<string, string>
+            {
+                {"bookName", bookName },
+                {"userID", userID },
+                {"quantity", quantity}
+            };
+
+            await TestAddCartItem(formData, HttpStatusCode.BadRequest);
+        }
+
+        [InlineData("Книга 1")]
+        [Theory]
+        public async Task Success_DeleteExist_CartItem_Async(string bookName)
+        {
+            var formData = new Dictionary<string, string>
+            {
+                {"cartItemDelete", bookName }
+            };
+
+            var content = new FormUrlEncodedContent(formData);
+
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
+
+            var response = await _fixture.Client.PostAsync("/api/cart/deleteCartItem", content);
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [InlineData("NotExistBook")]
+        [Theory]
+        public async Task Bad_DeleteNotExist_CartItem_Async(string bookName)
+        {
+            var formData = new Dictionary<string, string>
+            {
+                {"cartItemDelete", bookName }
+            };
+
+            var content = new FormUrlEncodedContent(formData);
+
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
+
+            var response = await _fixture.Client.PostAsync("/api/cart/deleteCartItem", content);
+
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [InlineData("2")]
+        [Theory]
+        public async Task Success_Clear_CartItems_Async(string userID)
+        {
+            var formData = new Dictionary<string, string>
+            {
+                {"userID", userID }
+            };
+
+            var content = new FormUrlEncodedContent(formData);
+
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
+
+            var response = await _fixture.Client.PostAsync("/api/cart/clearCart", content);
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [InlineData("-1")]
+        [Theory]
+        public async Task Bad_Clear_CartItems_Async(string userID)
+        {
+            var formData = new Dictionary<string, string>
+            {
+                {"userID", userID }
+            };
+
+            var content = new FormUrlEncodedContent(formData);
+
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
+
+            var response = await _fixture.Client.PostAsync("/api/cart/clearCart", content);
+
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        private async Task TestAddCartItem(Dictionary<string, string> formData, HttpStatusCode expectedStatusCode)
+        {
+            var content = new FormUrlEncodedContent(formData);
+
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
+
+            var response = await _fixture.Client.PostAsync($"/api/cart/addCartItem", content);
+
+            Assert.Equal(expectedStatusCode, response.StatusCode);
+        }
+    }
+}
